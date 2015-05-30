324577fc12d9397becdb43202df4fe38d53a617f
Amet13 <ametumerov@gmail.com>
2015-05-30 16:44:14 +0300

    Новый бланк. Поправил исходники

--- inc/ddos-deflate/ddos.sh
+++ inc/ddos-deflate/ddos.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-##############################################################################
-# DDoS-Deflate version 0.6 Author: Zaf <zaf@vsnl.com>                        #
-# It's fork of DDoS-Deflate by Amet13 <admin@amet13.name>                    #
-# https://github.com/Amet13/ddos-deflate                                     #
-##############################################################################
-# This program is distributed under the "Artistic License" Agreement         #
-#                                                                            #
-# The LICENSE file is located in the same directory as this program. Please  #
-# read the LICENSE file before you make copies or distribute this program    #
-##############################################################################
+#############################################################
+# DDoS-Deflate version 0.6 Author: Zaf <zaf@vsnl.com>
+# It's fork of DDoS-Deflate by Amet13 <admin@amet13.name> 
+# https://github.com/Amet13/ddos-deflate
+#############################################################
+# This program is distributed under the "Artistic License"
+# Agreement
+#
+# The LICENSE file is located in the same directory as
+# this program. Please read the LICENSE file before you
+# make copies or distribute this program
+#############################################################
 load_conf()
 {
 	CONF="/usr/local/ddos/ddos.conf"
@@ -25,7 +27,7 @@ head()
 {
 	echo "DDoS-Deflate version 0.6"
 	echo "Copyright (C) 2005, Zaf <zaf@vsnl.com>"
-	echo "It's fork of DDoS-Deflate by Amet13 <admin@amet13.name> "
+	echo "It's fork of DDoS-Deflate by Amet13 <admin@amet13.name>"
 	echo
 }
 
